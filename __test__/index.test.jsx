9b6538cbb7c1cfae49808d8534f5e82402005d69
Ricardo <33551688+ingkiller@users.noreply.github.com>
2022-03-14 17:46:58 -0400

    first test

---
+++ __test__/index.test.jsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react'
+import Footer from '../src/components/Footer/index'
+
+describe('Home', () =>{
+    it('renders a heading', () => {
+        render(<Footer/>)
+    })
+})
